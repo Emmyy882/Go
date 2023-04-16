9228119531bed99847a65cddcc342a8268afc148
Emmyy882 <emmaagbaje3@gmail.com>
2023-04-17 00:48:49 +0100

    getiing started

---
+++ go-programs/variables.go/variable.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age = 12
+	var name = "John"
+
+	fmt.Println("Name is ", name, "and age is ", age)
+}
---
+++ go-programs/variables1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var number = 45
+	var name = "John"
+
+	fmt.Println("Name is ", name, "number is ", number)
+}
