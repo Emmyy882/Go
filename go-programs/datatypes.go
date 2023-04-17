261155e2f808aac1e68a6919392ada2dfc5f7120
Emmyy882 <emmaagbaje3@gmail.com>
2023-04-17 21:56:34 +0100

    Program to Show go datatypes using variable declaration and thier corresponding values

---
+++ go-programs/datatypes.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var i int = 12
+	var j float32 = 8.2
+	var name string = "Mike"
+
+	fmt.Printf("%v, %T\n ", i, i)
+	fmt.Printf("%v, %T\n ", j, j)
+	fmt.Printf("%v, %T\n ", name, name)
+
+}
