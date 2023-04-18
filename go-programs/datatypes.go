600a3e96c52bfa422ba2a93df1a98b2669efcff2
Emmyy882 <emmaagbaje3@gmail.com>
2023-04-18 11:31:45 +0100

    updated the program

--- go-programs/datatypes.go
+++ go-programs/datatypes.go
@@ -4,12 +4,14 @@ import "fmt"
 
 func main() {
 
-	var i int = 12
-	var j float32 = 8.2
-	var name string = "Mike"
+	var i int = 12 //store whole numbers
+	var j float32 = 8.2 //stores fractional parts
+	var name string = "Mike" //stores group of characters
+	var light bool = false //can either be true or false
 
 	fmt.Printf("%v, %T\n ", i, i)
 	fmt.Printf("%v, %T\n ", j, j)
 	fmt.Printf("%v, %T\n ", name, name)
+	fmt.Printf("%v, %T\n ", light, light)
 
 }
