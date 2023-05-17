17ef67aa932bf11d127b62c6327150514b6d7ac4
Emmyy882 <emmaagbaje3@gmail.com>
2023-05-17 02:43:27 +0100

    test

---
+++ go-programs/SololearnChallenge/ withdraw.go
@@ -0,0 +1,5 @@
+/* TASK:
+you are working on a banking app.
+
+The code declares a BankAccount struct with a balance field.
+You need to add a withdraw() method for the BankAccount struct. It should take an integer argument and decrease the balance of the Bank Account by the given amount
